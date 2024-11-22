636ff4c10a4eb1894f7bf59a715380a2edaee3ba
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-11-22 17:19:47 +0530

    22-11-2024 all data uploaded

---
+++ module3/advanced-php/super_global_variables/what is super global variables.php
@@ -0,0 +1,18 @@
+<?php 
+/* 
+super global variables is used to access any variables globally.
+   types of super global variables ....
+
+   a) $_POST[""];
+   b) $_GET[""];
+   c) $_REQUEST[""];
+   d) $_SESSION[""];
+   e) $_COOKIE[""];
+   d) $_SERVER[""];
+   e) $_FILES[""];
+   f) $_ENV[""];
+
+*/
+
+
+?>
