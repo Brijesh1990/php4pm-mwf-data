5dc7cbd24879672973e0502972cd2cd6eac4ccec
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-11-29 17:02:12 +0530

    29-11-2024 all data uploaded

---
+++ module3/advanced-php/fruits-managements/content.php
@@ -0,0 +1,7 @@
+<div class="container bg-white shadow w-50 p-5 mt-5">
+    <h1>Fruits Managements Systems</h1>
+    <hr class="w-25">
+    <a href="manager.php"><button type="button" class="btn-lg btn btn-dark  p-4">Work As Manager<span class="bi bi-person"></span></button></a>
+  
+    <a href="customer.php"><button type="button" class="btn-lg btn btn-dark  p-4">Work As Customer<span class="bi bi-person-check"></span></button></a>
+</div>
